d6bbd9ee31fab6ebdc7551d522566c4363d9971a
QuentinDesbrousses <quentin.desbrousses@etu.umontpellier.fr>
2022-01-10 21:47:29 +0100

    update back

--- config/db.js
+++ config/db.js
@@ -1,11 +1,12 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
-    user:'cecile',
-    password:'aPres,pRèj',
-    database:'restaurangular',
-    host:'localhost',
+    user:process.env.db_user,
+    password:process.env.db_password,
+    database:process.env.db_database,
+    host:process.env.db_host,
     port : 5432
-});
+})
+
 
 pool.connect()
     .then(()=>console.log("connexion à la base de donnée réussie"))
